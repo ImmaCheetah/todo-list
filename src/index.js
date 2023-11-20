7728045de89c427d693fbde63a3c5ececdb3e6aa
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-20 14:08:31 -0500

    Create initial code for task and project to test functionality)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('it work');
+// Factory function to create a todo task
+function TodoTask(title, description, dueDate, priority, destination) {
+    title = title.toString();
+    description = description.toString();
+
+    
+    // console.log(typeof title);
+
+    return {title, description, dueDate, priority, destination};
+};
+
+const task1 = TodoTask('chores', 'wash dishes', 'nov 23', 'high');
+const task2 = TodoTask('chores', 'wash dishes', 'nov 23', 'high');
+const task3 = TodoTask('chores', 'wash dishes', 'nov 23', 'high');
+
+const testTask = TodoTask(4516, 'wash dishes', 'nov 23', 'high');
+
+const proj1 = Projects(testTask);
+
+// const projects = [
+//     {
+//         'project1': [task1, task2]
+//     },
+//     {
+//         'project2': [task3]
+//     }
+    
+// ]
+
+function Projects(task) {
+    let projects = [];
+
+    projects.push(task);
+
+    return {projects};
+}
+
+console.log(testTask);
+console.log(proj1);
+// console.log(projects[0].project1);
