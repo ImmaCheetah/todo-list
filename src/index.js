d1a89d7845767fb7c575db8c347ac0de5b530d9b
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-21 16:35:55 -0500

    Change Projects to use composition, doesn't work fully

--- src/index.js
+++ src/index.js
@@ -6,33 +6,48 @@ function TodoTask(title, description, dueDate, priority) {
 
     return {title, description, dueDate, priority};
 };
+function Inbox() {
+    let tasks = [];
 
-function Projects(title) {
-    let projectTasks = [];
-    
     const addTask = (newTaskName) => {
-        projectTasks.push(newTaskName);
+        tasks.push(newTaskName);
     }
 
     const displayTasks = () => {
-        for (let i = 0; i < projectTasks.length; i++) {
-            console.log(`Task ${i} - ${projectTasks[i].title}, ${projectTasks[i].description}, ${projectTasks[i].dueDate}, ${projectTasks[i].priority}`);
+        for (let i = 0; i < tasks.length; i++) {
+            console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].description}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
         }
-    }    
-
+    } 
     
-    return {title, projectTasks, addTask, displayTasks};
+    return {tasks, addTask, displayTasks};
 }
 
-function Inbox() {
-    let inboxTasks = [];
+function Projects(title) {
+    let tasks = [];
+    
+    const {addTask, displayTasks} = Inbox();
 
-    const addTask = (newTaskName) => {
-        inboxTasks.push(newTaskName);
+    function displayTasks() {
+        for (let i = 0; i < tasks.length; i++) {
+            console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].description}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
+        }
     }
     
-    return {inboxTasks, addTask};
+    // const addTask = (newTaskName) => {
+    //     projectTasks.push(newTaskName);
+    // }
+
+    // const displayTasks = () => {
+    //     for (let i = 0; i < projectTasks.length; i++) {
+    //         console.log(`Task ${i} - ${projectTasks[i].title}, ${projectTasks[i].description}, ${projectTasks[i].dueDate}, ${projectTasks[i].priority}`);
+    //     }
+    // }    
+
+    
+    return {title, tasks, addTask, displayTasks};
 }
+
+
 
 const mainInbox = Inbox();
 
@@ -40,13 +55,13 @@ const task1 = TodoTask('chores', 'wash dishes', 'nov 23', 'high');
 const task2 = TodoTask('movies', 'avatar', 'nov 29', 'med');
 const task3 = TodoTask('coding', 'todo list', 'dec 10', 'low');
 
-const proj1 = Projects("project 1", task1);
+const proj1 = Projects("project 1");
 const proj2 = Projects('project 2');
 
 
 proj1.addTask(task2);
 proj2.addTask(task1);
 mainInbox.addTask(task3);
-console.log(proj1.displayTasks());
-// console.log(proj2);
-// console.log(mainInbox);
+console.log(proj1);
+console.log(proj2);
+console.log(mainInbox);
