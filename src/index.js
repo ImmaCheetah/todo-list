840a3d458848254a1b16247dcb64b3282db5ca0f
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-22 18:07:48 -0500

    Add a getter to priority

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ function TodoTask(title, description, dueDate, priority) {
 
     title = title.toString();
     description = description.toString();
+    // priority = 'bop';
 
-    return {title, description, dueDate, priority};
+    const changePriority = (newPriority) => {
+        priority = newPriority;
+    }
+
+    return {
+        title, 
+        description, 
+        dueDate, 
+        get priority() {return priority;}, 
+        changePriority
+    };
 };
+
+//get priority(){return priority;}, set priority(value){priority = value},
 function Inbox() {
     let tasks = [];
 
@@ -18,20 +31,17 @@ function Inbox() {
             console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].description}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
         }
     } 
+
     
     return {tasks, addTask, displayTasks};
 }
 
 function Projects(title) {
-    let tasks = [];
+    tasks = [];
     
     const {addTask, displayTasks} = Inbox();
 
-    function displayTasks() {
-        for (let i = 0; i < tasks.length; i++) {
-            console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].description}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
-        }
-    }
+
     
     // const addTask = (newTaskName) => {
     //     projectTasks.push(newTaskName);
@@ -58,9 +68,10 @@ const task3 = TodoTask('coding', 'todo list', 'dec 10', 'low');
 const proj1 = Projects("project 1");
 const proj2 = Projects('project 2');
 
-
-proj1.addTask(task2);
-proj2.addTask(task1);
+task1.changePriority('low');
+console.log(task1);
+proj1.addTask(task1);
+proj2.addTask(task2);
 mainInbox.addTask(task3);
 console.log(proj1);
 console.log(proj2);
