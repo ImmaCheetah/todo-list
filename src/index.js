82f493a8773784b594745114a29bddd390deec79
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-23 10:30:07 -0500

    Remove description to be shown in displayTasks

--- src/index.js
+++ src/index.js
@@ -40,12 +40,10 @@ function Folder(title) {
 
     const displayTasks = () => {
         for (let i = 0; i < tasks.length; i++) {
-            console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].description}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
+            console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
         }
     }
 
-    
-    
     const deleteTask = (index) => {
         tasks.splice(index, 1);
     }
@@ -66,11 +64,11 @@ const task3 = Task('coding', 'todo list', 'dec 10', 'low');
 const proj1 = Folder("project 1");
 const proj2 = Folder('project 2');
 
+proj1.addTask(task1);
 task1.editTask('new thing', 'another new thing', 'new date', 'HIGH');
-console.log(task1.printTask());
+console.log(proj1.tasks[0].printTask());
 
 
-// proj1.addTask(task1);
 // proj1.addTask(task2);
 // proj1.deleteTask(1)
 // mainFolder.addTask(task3);
