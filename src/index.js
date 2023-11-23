a125352bcbbfaec917f71d289a42b7cec85044d1
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-11-22 21:54:59 -0500

    Add deleteTask function to Folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Factory function to create a todo task
 function Task(title, description, dueDate, priority) {
-
+    let id = 0
+    id++;
     title = title.toString();
     description = description.toString();
 
@@ -13,7 +14,8 @@ function Task(title, description, dueDate, priority) {
         description, 
         dueDate, 
         get priority() {return priority;}, 
-        changePriority
+        changePriority,
+        id
     };
 };
 
@@ -29,10 +31,14 @@ function Folder(title) {
         for (let i = 0; i < tasks.length; i++) {
             console.log(`Task ${i} - ${tasks[i].title}, ${tasks[i].description}, ${tasks[i].dueDate}, ${tasks[i].priority}`);
         }
-    } 
+    }
+    
+    const deleteTask = (index) => {
+        tasks.splice(index, 1);
+    }
 
     
-    return {title, tasks, addTask, displayTasks};
+    return {title, tasks, addTask, displayTasks, deleteTask};
 }
 
 
@@ -47,16 +53,17 @@ const task3 = Task('coding', 'todo list', 'dec 10', 'low');
 const proj1 = Folder("project 1");
 const proj2 = Folder('project 2');
 
-task1.changePriority('low');
-// console.log(task1);
+
+
+
 proj1.addTask(task1);
 proj1.addTask(task2);
+proj1.deleteTask(1)
 mainFolder.addTask(task3);
 console.log(proj1);
 console.log(mainFolder);
 console.log(proj1.displayTasks());
-proj1.tasks[0].changePriority('HIGH');
-console.log(proj1.displayTasks());
+
 
 
 // function Projects(title) {
