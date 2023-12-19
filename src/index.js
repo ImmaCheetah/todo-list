a0053fb4e9e8f66540657a1ee8523b218013c4f1
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-12-19 13:57:26 -0500

    Add comments and function to display tasks after match made

--- src/index.js
+++ src/index.js
@@ -38,13 +38,12 @@ inboxFolder.displayTasks();
 superFolder.addFolder(inboxFolder);
 console.log(superFolder);
 
-
-const addBtn = document.getElementById('task-add-btn');
-
 const taskDialog = document.getElementById('task-dialog');
 
 taskDialog.show();
 
+const addBtn = document.getElementById('task-add-btn');
+// Create new task instance using info from form
 addBtn.addEventListener('click', function() {
     // This version uses arrays
     // let taskDetails = getTaskFormInfo();
@@ -58,8 +57,8 @@ addBtn.addEventListener('click', function() {
 displayFolderTasks(inboxFolder);
 // displayFolders(superFolder);
 
+// Open folder modal when clicked
 const addFolderBtn = document.getElementById('folder-add-btn');
-
 addFolderBtn.addEventListener('click', function() {
     
     const folderDialog = document.getElementById('folder-dialog');
@@ -68,10 +67,12 @@ addFolderBtn.addEventListener('click', function() {
     folderDialog.showModal();
 });
 
-const folderSubmitBtn = document.getElementById('folder-confirm-btn');
-
 // Event listener to submit folder form
+// Create a new folder instance with info from form
+// Add to DOM
+// Add to super folder
 // Should be done on the form instead of button but MDN example with dialog did it this way
+const folderSubmitBtn = document.getElementById('folder-confirm-btn');
 folderSubmitBtn.addEventListener('click', function(e) {
     e.preventDefault();
     
@@ -87,7 +88,8 @@ folderSubmitBtn.addEventListener('click', function(e) {
     folderDialog.close();
 });
 
-
+// Go through all folders and check if the clicked button value matches folder value
+// then display tasks of that folder to screen
 function openFolderWithID() {
     console.log(this.value);
 
@@ -95,10 +97,12 @@ function openFolderWithID() {
     superFolder.folders.forEach(folder => {
         if (tempId === folder.myuuid) {
             //display all tasks of this folder to page
+            displayFolderTasks(folder);
             console.log("match found");
         }
     });
 }
+
 
 
 export {
